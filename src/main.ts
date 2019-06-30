6021b9cf2472962e56307f9cca1c036c790ec330
moshiour <moshiour.rahman@ackosoft.com>
2019-06-30 12:00:06 +0600

    Hot Module Replacement Configuration added in project and Packages updated with npm update.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,46 @@
-import {enableProdMode} from '@angular/core';
+import {ApplicationRef, enableProdMode, NgModuleRef} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
+import {createNewHosts} from '@angularclass/hmr';
+
+interface HotModule extends NodeModule {
+  hot: {
+    accept();
+    dispose(fn);
+  };
+}
 
 if (environment.production) {
   enableProdMode();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch(console.error);
-});
+const hmrBootstrap = ({hot}: HotModule, bootstrap: () => Promise<NgModuleRef<any>>) => {
+  let ngModule: NgModuleRef<any>;
+  hot.accept();
+  bootstrap().then(mod => ngModule = mod);
+  hot.dispose(() => {
+    const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
+    const elements = appRef.components.map(c => c.location.nativeElement);
+    const makeVisible = createNewHosts(elements);
+    ngModule.destroy();
+    makeVisible();
+  });
+};
+
+
+const bootstrapFn = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (environment.hmr) {
+  if ((module as HotModule).hot) {
+    hmrBootstrap(module as HotModule, bootstrapFn);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  document.addEventListener('DOMContentLoaded', () => {
+    bootstrapFn().catch(console.error);
+  });
+}
