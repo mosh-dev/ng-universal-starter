855473df4da11d59aa5ea250ea4e77df011e721a
Moshiour <moshiour.rahman@ackosoft.com>
2019-09-10 13:50:25 +0600

    Comment update in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,28 @@ import {AppComponent} from './app.component';
 
 @NgModule({
   /**
-   * Makes Everything PRIVATELY available to this module
+   * To use any Pipe,Component,Directive they need to be declared inside a module
+   * They can be declared to only one module.
    * HOLDS: Components, Directives and Pipes.
    */
   declarations: [AppComponent],
   /**
-   * To access other modules exported components, directives and pipes import is used.
+   * To use other modules exported components, directives and pipes.
    * HOLDS: modules
    */
   imports: [
     BrowserModule.withServerTransition({appId: 'ng-universal-starter'})
   ],
   /**
-   * Makes Everything PUBLICLY available to this module and other Module that Imports it.
+   * Makes Everything PUBLICLY available to other Module that Imports it.
    * We should really not export anything from app module for better project Organization.
-   * HOLDS: Components, Directives and Pipes.
+   * HOLDS: Components, Directives, Pipes and Modules.
    */
   exports: [],
   /**
    * Register data providers such as SERVICES and GUARDS.
-   * These things can be Injected within this module Only
-   * The Injected class will be available to any component in this module and any module that imports it.
+   * If not provided in root via @injectable metadata, These things can be Injected within this module Only
+   * The Injected class will be available in this module and any module that imports it.
    * HOLDS: services and guards
    */
   providers: [],
@@ -36,7 +37,8 @@ import {AppComponent} from './app.component';
   bootstrap: [AppComponent],
   /**
    * Components that is loaded dynamically on runtime
-   * and that are not used in any route or template should be declared Here, Since they will be tree Otherwise
+   * and that are not used in any route or template should be declared Here,
+   * Otherwise treeShaking will remove them in prod build.
    * HOLDS: component
    */
   entryComponents: []
