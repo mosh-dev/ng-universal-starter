cac274c88a8536dd2729398ca61960b803d26551
Moshiour <moshiour.rahman@ackosoft.com>
2019-09-28 14:48:07 +0600

    Timer Utility Function Added

--- src/utilities/utility.ts
+++ src/utilities/utility.ts
@@ -1,4 +1,6 @@
 import {NODE_PLATFORM} from './platform';
+import {timer} from 'rxjs';
+import {map, takeUntil} from 'rxjs/operators';
 
 export const fromBase64 = (base64String: string) => {
   return NODE_PLATFORM ? Buffer.from(base64String, 'base64').toString() : atob(base64String);
@@ -6,4 +8,25 @@ export const fromBase64 = (base64String: string) => {
 
 export const toBase64 = (value: string) => {
   return NODE_PLATFORM ? Buffer.from(value).toString('base64') : btoa(value);
+};
+
+export const getTimer = (hour: number) => {
+  /**
+   * Get today's date and time
+   */
+  const NOW = new Date().getTime();
+  const DUE_TIME = (hour * 60 * 60 * 1000) - 1000;
+  let distance = NOW + DUE_TIME;
+
+  return timer(0, 1000).pipe(
+    takeUntil(timer(DUE_TIME)),
+    map(() => {
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      distance -= 1000;
+      return `${days ? days + 'd' : ''} ${hours ? hours + 'h' : ''} ${minutes ? minutes + 'm' : ''} ${seconds ? seconds + 's' : ''}`;
+    })
+  );
 };
