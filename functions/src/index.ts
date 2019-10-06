1061cc10af963b6c7feb68e85463b74065d76d4b
moshiour <moshiour.rahman@ackosoft.com>
2019-10-06 16:57:00 +0600

    Functions index.ts updated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,10 @@
-import * as functions from 'firebase-functions';
+/**
+ * Run Npm I inside functions
+ * Remove Comments
+ */
 
-const universal = require(`${process.cwd()}/dist/server`).app;
-
-export const ssr = functions.https.onRequest(universal);
+// import * as functions from 'firebase-functions';
+//
+// const universal = require(`${process.cwd()}/dist/server`).app;
+//
+// export const ssr = functions.https.onRequest(universal);
