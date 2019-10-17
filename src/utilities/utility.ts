ae94731f2e809ee22b6295c2a2fa88169dc4d65f
moshiour <moshiour.rahman@ackosoft.com>
2019-10-17 16:30:29 +0600

    One Utility Function Added

--- src/utilities/utility.ts
+++ src/utilities/utility.ts
@@ -1,6 +1,7 @@
 import {NODE_PLATFORM} from './platform';
 import {timer} from 'rxjs';
 import {map, takeUntil} from 'rxjs/operators';
+import {AbstractControl, FormArray, FormControl, FormGroup} from '@angular/forms';
 
 export const fromBase64 = (base64String: string) => {
   return NODE_PLATFORM ? Buffer.from(base64String, 'base64').toString() : atob(base64String);
@@ -23,4 +24,40 @@ export const getTimer = (hour: number) => {
       return `${days ? days + 'd' : ''} ${hours ? hours + 'h' : ''} ${minutes ? minutes + 'm' : ''} ${seconds ? seconds + 's' : ''}`;
     })
   );
+};
+
+/**
+ * Build Reactive Form Errors Recursively
+ * It Does Not Support Multi Level Deep FormArray
+ * It Also Does Replace Errors with Same Control Name With Different Group
+ */
+interface ReactiveErrorItem {
+  controlName: string;
+  errors: any;
+}
+
+interface FormControls {
+  [key: string]: AbstractControl;
+}
+
+export const buildReactiveFormErrors = (controls: FormControls, errs = []): Array<ReactiveErrorItem> => {
+  Object
+    .entries(controls)
+    .forEach(([controlName, control]) => {
+      if (control instanceof FormGroup) {
+        if (control.errors) {
+          errs.push({[controlName]: controlName, errors: control.errors});
+        }
+        buildReactiveFormErrors(control.controls, errs);
+      } else if (control instanceof FormArray) {
+        errs.push({
+          [controlName]: controlName,
+          errors: control.controls.map(({errors}) => errors).filter(e => e)
+        });
+      } else if (control instanceof FormControl && control.errors) {
+        errs.push({[controlName]: controlName, errors: control.errors});
+      }
+    });
+  return errs
+    .filter(({errors}) => (errors instanceof Object) ? Object.keys(errors).length : errors ? errors.length : false);
 };
