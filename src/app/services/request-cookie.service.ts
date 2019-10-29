843fcc160c831386e76fec03ac3abe27f18dcb07
moshiour <moshiour.rahman@ackosoft.com>
2019-10-29 13:07:17 +0600

    CookieService Typings Added

--- src/app/services/request-cookie.service.ts
+++ src/app/services/request-cookie.service.ts
@@ -2,6 +2,7 @@ import {Inject, Injectable, Optional} from '@angular/core';
 import {fromBase64} from '../../utilities/utility';
 import {BrowserCookieStorage} from '../../utilities/storage/cookieStorage';
 import {NODE_PLATFORM} from '../../utilities/platform';
+import {ObjectMap} from '../../typings/typings';
 
 @Injectable({providedIn: 'root'})
 export class RequestCookieService {
@@ -10,16 +11,16 @@ export class RequestCookieService {
    * These keys are filtered from cookie object
    */
   blackListedKeys: string[] = [];
-  private cookieStore: any;
+  private cookieStore: ObjectMap;
 
-  constructor(@Optional() @Inject('COOKIES') private serverCookies: any) {
+  constructor(@Optional() @Inject('COOKIES') private serverCookies: ObjectMap) {
   }
 
   /**
    * All the cookies are base64 Encoded
    * So We are Decoding It On the fly
    */
-  private get decodedCookies(): any {
+  private get decodedCookies(): ObjectMap {
     if (!this.cookieStore) {
       const SERVER_COOKIES = this.serverCookies || {};
       this.cookieStore = Object
@@ -38,7 +39,7 @@ export class RequestCookieService {
     return this.cookieStore || {};
   }
 
-  getCookieItem(name): any {
+  getCookieItem(name) {
     return NODE_PLATFORM ? this.decodedCookies[name] : BrowserCookieStorage.getItem(name);
   }
 }
--- src/typings/typings.d.ts
+++ src/typings/typings.d.ts
@@ -1,2 +1,6 @@
 type DayOfTheWeek = 'sunday' | 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday';
 type WeekMap<T = number | string> = Partial<{ [day in DayOfTheWeek]: T }>;
+
+export interface ObjectMap {
+  [key: string]: any;
+}
