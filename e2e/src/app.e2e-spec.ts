1b4bd66633cfb9320c9fc266d982c9587e0bd0b7
Moshiour Rahman <39358862+mosh-ackosoft@users.noreply.github.com>
2020-05-09 18:16:14 +0600

    e2e test Corrected for landing page

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ng-universal-starter app is running!');
+    expect(page.getTitleText()).toEqual('ng-universal-starter app is not running!');
   });
 
   afterEach(async () => {
