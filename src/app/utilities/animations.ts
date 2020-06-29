44f6c07ff326b59fcf3fb27fa4808eec42bbb962
Moshiour Rahman <39358862+mosh-ackosoft@users.noreply.github.com>
2020-06-29 10:09:27 +0600

    Users Module Updated

--- src/app/utilities/animations.ts
+++ src/app/utilities/animations.ts
@@ -1,4 +1,4 @@
-import {animate, keyframes, query, stagger, state, style, transition, trigger} from '@angular/animations';
+import { animate, keyframes, query, stagger, state, style, transition, trigger } from '@angular/animations';
 
 export const fadeIn = [
   trigger('fadeIn', [
@@ -23,7 +23,7 @@ export const fade = [
 
 
 export const slideIn = [
-  trigger('slide', [
+  trigger('slideIn', [
     state('left', style({transform: 'translateX(0)'})),
     state('right', style({transform: 'translateX(-50%)'})),
     transition('* => *', animate(300))
@@ -34,28 +34,15 @@ export const listAnimation = [
   trigger('listAnimation', [
     transition('* => *', [
       query(':enter', style({opacity: 0}), {optional: true}),
-      query(':enter', stagger('100ms', [
-        animate('150ms ease-in', keyframes([
+      query(':enter', stagger('10ms', [
+        animate('300ms ease-in', keyframes([
           style({opacity: 0, transform: 'translateX(-75%)', offset: 0}),
           style({opacity: .5, transform: 'translateX(-35px)', offset: 0.3}),
           style({opacity: 1, transform: 'translateX(0)', offset: 1.0}),
         ]))]), {optional: true}),
-      query(':leave', stagger('100ms', [
-        animate('150ms ease-in', keyframes([
+      query(':leave', stagger('10ms', [
+        animate('300ms ease-in', keyframes([
           style({opacity: 1, transform: 'translateX(0)', offset: 0}),
-          style({opacity: 0, transform: 'translateX(75%)', offset: 1.0}),
-        ]))]), {optional: true})
-    ])
-  ])
-];
-
-
-export const listAnimationLeave = [
-  trigger('listAnimationLeave', [
-    transition('* => *', [
-      query(':leave', stagger('100ms', [
-        animate('150ms ease-in', keyframes([
-          style({opacity: 1, transform: 'translateY(0)', offset: 0}),
           style({opacity: 0, transform: 'translateX(75%)', offset: 1.0}),
         ]))]), {optional: true})
     ])
